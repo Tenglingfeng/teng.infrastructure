f735c38e0b11ddda45cce5ae20f11d005034e3b5
Teng <704279465@qq.com>
2021-09-12 13:35:25 +0800

    用户登录接口

--- src/Teng.Infrastructure.HttpApi.Host/InfrastructureHttpApiHostModule.cs
+++ src/Teng.Infrastructure.HttpApi.Host/InfrastructureHttpApiHostModule.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
---
+++ src/Teng.Infrastructure.HttpApi/Models/LoginResultType.cs
@@ -0,0 +1,15 @@
+﻿namespace Teng.Infrastructure.Models
+{
+    public enum LoginResultType : byte
+    {
+        Success = 1,
+
+        InvalidUserNameOrPassword = 2,
+
+        NotAllowed = 3,
+
+        LockedOut = 4,
+
+        RequiresTwoFactor = 5
+    }
+}
